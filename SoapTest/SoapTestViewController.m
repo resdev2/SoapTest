ead60252a6d8365df671ddc484a277c7292fff14
Mugdha <mk.mugdha@gmail.com>
2011-10-11 01:17:41 -0400

    Git test with change

--- SoapTest/SoapTestViewController.m
+++ SoapTest/SoapTestViewController.m
@@ -3,7 +3,7 @@
 //  SoapTest
 //
 //  Created by Mugdha Kulkarni on 9/20/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Birdgeware Systems Inc. All rights reserved.
 //
 
 #import "SoapTestViewController.h"
